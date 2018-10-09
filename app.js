9edac7f7b616f82b8c7178207de931e6c02b7c97
Rishi Raj <rishiraj6686@gmail.com>
2018-10-09 07:51:59 +0530

    Removed server port typo

--- app.js
+++ app.js
@@ -133,5 +133,5 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP ||  '0.0.0.0';
 
 app.listen(server_port,server_ip_address, function () {
-    console.log('App listening on ' + port)
+    console.log('App listening on ' + server_ip_address + ":" + server_port)
 })
